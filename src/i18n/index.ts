2e00c90d866aa9e65e6ae8e2cd0945921898b5da
Yuankai_Chen <Yuankai_Chen@epam.com>
2023-02-14 17:43:46 +0800

    lang json export

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,10 +1,14 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
+import langTrans, { LANGUAGES } from "./languages";
+
 i18n.use(initReactI18next).init({
-  resources: {},
-  lng: "en",
-  fallbackLng: "en",
+  resources: {
+    ...langTrans,
+  },
+  lng: LANGUAGES.en,
+  fallbackLng: LANGUAGES.cn,
   interpolation: {
     escapeValue: false,
   },
---
+++ src/i18n/languages/index.ts
@@ -0,0 +1,9 @@
+import cn from "./cn/cn.json";
+import en from "./en/en.json";
+
+export default { ...cn, ...en };
+
+export const LANGUAGES = {
+  en: "en",
+  cn: "cn",
+};
